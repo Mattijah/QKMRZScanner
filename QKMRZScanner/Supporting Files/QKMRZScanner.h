1b5907648529400f6a2e7aa7ec8b930c5247cab1
Mattijah <Mattijah@users.noreply.github.com>
2018-10-03 20:19:36 +0200

    Initial Commit

---
+++ QKMRZScanner/Supporting Files/QKMRZScanner.h
@@ -0,0 +1,18 @@
+//
+//  QKMRZScanner.h
+//  QKMRZScanner
+//
+//  Created by S on 03/10/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QKMRZScanner.
+FOUNDATION_EXPORT double QKMRZScannerVersionNumber;
+
+//! Project version string for QKMRZScanner.
+FOUNDATION_EXPORT const unsigned char QKMRZScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QKMRZScanner/PublicHeader.h>
+
+
