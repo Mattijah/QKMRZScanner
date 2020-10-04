4d184820db12f6e62d5c189e4b5caeb37ad75885
Mattijah <Mattijah@users.noreply.github.com>
2020-10-04 22:21:35 +0200

    Change the engine mode to silent warning
    - With the lstmCombined engine we're getting "Error: LSTM requested, but not present" warning as the file does not support LSTM.

--- QKMRZScanner/QKMRZScannerView.swift
+++ QKMRZScanner/QKMRZScannerView.swift
@@ -18,7 +18,7 @@ public protocol QKMRZScannerViewDelegate: class {
 
 @IBDesignable
 public class QKMRZScannerView: UIView {
-    fileprivate let tesseract = SwiftyTesseract(language: .custom("ocrb"), dataSource: Bundle(for: QKMRZScannerView.self), engineMode: .tesseractLstmCombined)
+    fileprivate let tesseract = SwiftyTesseract(language: .custom("ocrb"), dataSource: Bundle(for: QKMRZScannerView.self), engineMode: .tesseractOnly)
     fileprivate let mrzParser = QKMRZParser(ocrCorrection: true)
     fileprivate let captureSession = AVCaptureSession()
     fileprivate let videoOutput = AVCaptureVideoDataOutput()
